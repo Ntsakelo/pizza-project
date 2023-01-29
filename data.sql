c9d5fe11c1af7f9da05792eb19feef9ec05557de
Ntsakelo <Ntsakeloarnold00@gmail.com>
2023-01-29 15:53:27 +0200

    changes

---
+++ data.sql
@@ -0,0 +1,12 @@
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Bacon cheese',29.99,56.87,100.00,'Bacon,Cheese, Chicken Sauce','https://cdn.pixabay.com/photo/2016/03/05/21/47/american-1239091_960_720.jpg');
+
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Italian mediterranean',26.00,47.87,89.99,'Tomato,Cheese','https://cdn.pixabay.com/photo/2018/04/17/17/15/italian-3328122_960_720.png');
+
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Ham cheese',37.99,60.00,110.00,'Ham,Cheese','https://cdn.pixabay.com/photo/2017/01/03/11/33/pizza-1949183_960_720.jpg');
+
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Basil tropica',44.00,75.00,115.00,'Basil,Olives,Tomato','https://cdn.pixabay.com/photo/2015/12/07/22/27/pizza-1081534_960_720.jpg');
+
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Italian cheese',40.99,55.00,110.00,'Cheese,Bacon,Sauce','https://cdn.pixabay.com/photo/2016/02/16/07/39/pizza-1202775_960_720.jpg');
+
+INSERT INTO products(product,small_price,medium_price,large_price,ingredients,image_url) VALUES ('Cheese tomato',30.99,40.00,90.00,'Cheese,Tomato sauce','https://cdn.pixabay.com/photo/2017/11/26/08/42/pizza-2978377_960_720.jpg');
+
---
+++ tables.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS catalogue;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
+
+create table customers(
+    id serial primary key not null,
+    firstname text not null,
+    lastname text not null,
+    email text not null,
+    password text not null
+);
+
+create table products(
+    id serial primary key not null,
+    product text not null,
+    small_price decimal not null,
+    medium_price decimal not null,
+    large_price decimal not null,
+    ingredients text not null,
+    image_url text not null
+);
+
+create table catalogue(
+    id serial primary key not null,
+    product_id int not null,
+    session_id text not null,
+    size text not null,
+    price text not null,
+    qty int not null,
+    foreign key(product_id) references products(id) on delete cascade
+);
+
+create table orders(
+    id serial primary key not null,
+    customer_id int not null,
+    product text not null,
+    qty int not null,
+    order_date time not null,
+    order_status text not null,
+    foreign key(customer_id) references customers(id) on delete cascade
+);
